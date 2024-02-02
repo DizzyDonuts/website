782fd448651ecbfecef59bd520d501c111fdc892
Jon Ilko <jilko@xtivia.com>
2024-02-02 16:15:22 -0500

    image update

--- contentful.config.js
+++ contentful.config.js
@@ -13,12 +13,12 @@ module.exports = {
       .replace('http://', '')}`,
     url,
     image:
-      'https://images.ctfassets.net/w8vf7dk7f259/4bucno7z1xAyVI5MOkU6Pu/ded83d0ec1eb732ae3a81ddab7a18877/fallback-image-03.jpg',
+      'https://https://images.ctfassets.net/8zvqegep4uuw/6HhdAB3AAUDj65T7phr0xN/ec8d0c58e24e0c15c228e835b3a33283/DE5A4D9F-12A4-493F-9B28-AA95744A7B9C.jpeg',
   },
   icon: {
     light:
-      'https://images.ctfassets.net/w8vf7dk7f259/llZXwDCnl9NqdyuVvjn1n/d20cea90225e7f53dfbf8a18a46e972d/gocoin-icon-light.svg',
-    dark: 'https://images.ctfassets.net/w8vf7dk7f259/i9iu6GU6dFWQJJwJzwxCT/952cc3bab415e28f521c22933072a09c/gocoin-icon.svg',
+      'https://images.ctfassets.net/8zvqegep4uuw/5jk1cMU9fxlguLLpn1LXgl/5dfd9f0acb938ce53781bfce4cc76173/favicon-32x32.svg',
+    dark: 'https://images.ctfassets.net/8zvqegep4uuw/5jk1cMU9fxlguLLpn1LXgl/5dfd9f0acb938ce53781bfce4cc76173/favicon-32x32.svg',
     width: 66,
     height: 64,
   },
